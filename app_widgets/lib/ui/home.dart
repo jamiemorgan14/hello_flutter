8334b11afe1a77772c399a9a7fd5fa1ad90a032b
Jamie Morgan <jmorgan3475@gmail.com>
2020-11-23 16:08:37 -0700

    stateful

--- app_widgets/lib/ui/home.dart
+++ app_widgets/lib/ui/home.dart
@@ -7,19 +7,107 @@ class Home extends StatelessWidget {
       child: Container(
         alignment: Alignment.center,
         color: Colors.deepOrangeAccent,
-        child: Text(
-          'Pizza',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-              fontSize: 80.0,
-              decoration: TextDecoration.none,
-              fontFamily: 'Oxygen',
-              fontWeight: FontWeight.normal),
+        padding: EdgeInsets.only(top: 30.0, left: 10.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    'Margherita',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 30.0,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Oxygen',
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'Tomato, Mozzarella, Basil',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Oxygen',
+                        fontWeight: FontWeight.normal),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    'Marinara',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 30.0,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Oxygen',
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'Tomato, Garlic',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Oxygen',
+                        fontWeight: FontWeight.normal),
+                  ),
+                )
+              ],
+            ),
+            PizzaImageWidget(),
+            OrderButton(),
+          ],
+
+          // width: 192.0,
+          // height: 96.0,
+          // margin: EdgeInsets.only(left: 50.0)
         ),
-        // width: 192.0,
-        // height: 96.0,
-        // margin: EdgeInsets.only(left: 50.0)
       ),
     );
+  }
+}
+
+class PizzaImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaAsset = AssetImage('images/meltypizza.png');
+    Image image = Image(image: pizzaAsset, width: 250.0, height: 250.0);
+    return Container(
+      child: image,
+      padding: EdgeInsets.only(top: 20.0),
+    );
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+        child: Text("Order your Pizza!"),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
+      ),
+    );
+  }
+
+  void order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for your order"),
+    );
+    showDialog(context: context, builder: (BuildContext context) => alert);
   }
 }
