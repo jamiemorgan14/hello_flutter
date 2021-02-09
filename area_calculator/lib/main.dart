cb71effe559fa6230618291c7706433b7d02ac2b
Jamie Morgan <jmorgan3475@gmail.com>
2021-02-08 20:58:48 -0700

    shape builder

--- area_calculator/lib/main.dart
+++ area_calculator/lib/main.dart
@@ -44,7 +44,8 @@ class _AreaCalculatorState extends State<AreaCalculator> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(top: 15.0),
+      margin: EdgeInsets.only(top: 15.0),
+      child: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             //dropdown
@@ -64,6 +65,10 @@ class _AreaCalculatorState extends State<AreaCalculator> {
                     currentShape = shape;
                   });
                 }),
+
+            //shape container
+            ShapeContainer(shape: currentShape),
+
             //width
             AreaTextField(controller: widthController, hint: 'Width'),
             //height
@@ -87,7 +92,9 @@ class _AreaCalculatorState extends State<AreaCalculator> {
               ),
             ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 
   void calculateArea() {
@@ -150,5 +157,61 @@ class AreaTextField extends StatelessWidget {
             hintText: hint,
           ),
         ));
+  }
+}
+
+class ShapeContainer extends StatelessWidget {
+  final String shape;
+
+  const ShapeContainer({Key key, this.shape}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    if (shape == 'Triangle') {
+      return CustomPaint(
+        size: Size(100, 100),
+        painter: TrianglePainter(),
+      );
+    } else {
+      return CustomPaint(
+        size: Size(100, 100),
+        painter: RectanglePainter(),
+      );
+    }
+  }
+}
+
+class TrianglePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.deepOrange;
+    var path = Path();
+    path.moveTo(size.width / 2, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class RectanglePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.deepPurple;
+    Rect rect =
+        Rect.fromLTRB(0, size.height / 4, size.width, size.height / 4 * 3);
+    canvas.drawRect(rect, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
   }
 }
